0f444aa289976ea789ea9b558a9aad1011e7bd21
Rogerio <rogerio.dasilva@formstack.com>
2023-12-22 17:54:22 -0500

    expose url for static element

--- lwr-react-salesforce/force-app/main/default/lwc/testLWCFirstComponent/testLWCFirstComponent.js
+++ lwr-react-salesforce/force-app/main/default/lwc/testLWCFirstComponent/testLWCFirstComponent.js
@@ -5,7 +5,7 @@ import LWR_CONTAINER_URL from "@salesforce/resourceUrl/LWRContainer";
 export default class TestLWCFirstComponent extends LightningElement {
   @api buttonText;
 
-  mainLWRStaticURL = LWR_CONTAINER_URL + '/lwr-engine.js';
+  mainLWRStaticURL = LWR_CONTAINER_URL + '/custom-javascript/lwr-engine.js';
 
   buttonClickEvent() {
     alert("Hello");
